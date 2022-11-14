971ac684203ed8ff3fda6c2ceda36329a52e82db
leandrosardi <leandro.sardi@expandedventure.com>
2022-11-14 18:49:30 -0300

    parsing sales navigator search results pages

--- examples/parse.rb
+++ examples/parse.rb
@@ -15,19 +15,30 @@ end
 exit(0)
 =end
 
-doc = Nokogiri::HTML(open('./sales-navigator-result-pages/1.html'))
-
-lis = doc.xpath('//li[contains(@class, "artdeco-list__item")]')
+output_file = './sales-navigator-result-pages/results.csv' # the output file
+output = File.open(output_file, 'w')
 
 i = 0
-lis.each { |li|
-    i += 1
-    doc2 = Nokogiri::HTML(li.inner_html)
-    n1 = doc2.xpath('//div[contains(@class,"artdeco-entity-lockup__title")]/a/span').first
-    n2 = doc2.xpath('//div[contains(@class,"artdeco-entity-lockup__subtitle")]/a').first
-    print "#{i.to_s}"
-    print " - #{n1.text.strip}" if !n1.nil?
-    print " - #{n2.text.strip}" if !n2.nil?
-    puts
-}
+source = "./sales-navigator-result-pages/*.html" # the files to be imported
+Dir.glob(source).each do |file|
+    doc = Nokogiri::HTML(open(file))
+    lis = doc.xpath('//li[contains(@class, "artdeco-list__item")]')    
+    lis.each { |li|
+        i += 1
+        doc2 = Nokogiri::HTML(li.inner_html)
+        n1 = doc2.xpath('//div[contains(@class,"artdeco-entity-lockup__title")]/a/span').first
+        n2 = doc2.xpath('//div[contains(@class,"artdeco-entity-lockup__subtitle")]/a').first
+        print "#{i.to_s}"
+        line = []
+        line << "\"#{n1.text.strip.gsub('"', '')}\"" if !n1.nil?
+        line << "\"#{n2.text.strip.gsub('"', '')}\"" if !n2.nil?
+        print ", #{line.join(',')}"
+        output.puts line.join(',')
+        output.flush
+        puts
+    }    
+end
+
+output.close
+
 

80a10ff1709f493fcfae7b93783c74abe9e0b9e9
leandrosardi <leandro.sardi@expandedventure.com>
2022-11-15 19:14:49 -0300

    #1

--- popup.js
+++ popup.js
@@ -2,7 +2,33 @@
 
 function changeBodyForTesting() {
     alert('changeBodyForTesting');
-    document.body.innerHTML = 'Hello!';
+    document.body.innerHTML = '...';
+    // do an ajax call to https://connectionsphere.com/api1.0/emails/verify.json?email=leandro.sardi@expandedventure.com, and get the response
+    // if response is 200, then change the body to 'Verified'
+    // if response is 400, then change the body to 'Not Verified'
+    /*
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        document.body.innerHTML = 'Hello!';
+    }
+    xhttp.open("GET", "https://connectionsphere.com/api1.0/emails/verify.json", true);
+    xhttp.send();
+    */
+    let apiCall = 'https://connectionsphere.com/api1.0/emails/verify.json?email=leandro.sardi@expandedventure.com';
+    fetch(apiCall).then(function(res) {
+        // wait for resonse
+        if (res.status !== 200) {
+            document.body.innerHTML = 'Error!';
+            return;
+        }
+        res.json().then(function(data) {
+            // show the response
+            document.body.innerHTML = data;
+        });
+    }).catch(function(err) {
+        // error
+        document.body.innerHTML = err;
+    });
 }
 
 postsSearchStart.onclick = function() {

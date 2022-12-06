d1202bca10d325f403d685ecfa3b5f8d5d40ed5a
leandrosardi <leandro.sardi@expandedventure.com>
2022-12-06 14:21:48 -0300

    #7

--- popup.js
+++ popup.js
@@ -1,36 +1,34 @@
 'use strict';
 
-const url = 'https://127.0.0.1';
+const csdomain = 'https://127.0.0.1';
 
 let email = document.getElementById('email');
 let password = document.getElementById('password');
 
-function do_login(url, email, password) {
-    // remove any existing div#status
-    let status = document.getElementById('status');
-    if (status) status.remove();
-    // inject the status top div
-    let div = document.createElement('div');
-    div.id = 'status';
-    div.style = 'position: fixed; top: 0; left: 0; width: 100%; height: 50px; background-color: rgba(255, 255, 255, 0.5); color: black; z-index: 9999;';
-    document.body.appendChild(div);
-    // show loading status
-    div.innerHTML = 'Loading...';
-    // define the api URL
-    let apiCall = url+'/api1.0/isn/login.json?email='+email+'&password='+password;
-    //let ret = '';
-    //let p = new Promise(function() {
+function do_login(csdomain, email, password) {
+    function get_page(csdomain) {
+        alert('get_page');
+        id_login = document.getElementById('id_login');
+        id_page = document.getElementById('id_page');
+        url = document.getElementById('url');
+        text = document.getElementById('text');
+        // show loading status
+        text.innerHTML = 'Getting page to scrape...';
+        // define the api URL
+        let apiCall = csdomain+'/api1.0/isn/get.json?id_login='+id_login.value
         fetch(apiCall).then(function(res) {
             // wait for resonse
             if (res.status !== 200) {
-                div.innerHTML = 'Protocol Error';
+                text.innerHTML = 'Protocol Error';
             }
             res.json().then(function(data) {
                 // show the response
                 if (data.status === 'success') {
-                    div.innerHTML = 'Res:'+data.id_login;
+                    text.innerHTML = 'Res:'+data.status;
+                    id_page.value = data.id_page;
+                    url.value = data.url;
                 } else {
-                    div.innerHTML = 'Res:'+data.status;
+                    text.innerHTML = 'Res:'+data.status;
                 }
             });
         }).catch(function(err) {
@@ -40,16 +38,75 @@ function do_login(url, email, password) {
             // In this case, go to https://127.0.0.1/ on other tab of the same browser, and choose to trust the certificate.
             // After that, you can go back to this tab and try again.
             // 
-            div.innerHTML = 'Error:'+err;
+            text.innerHTML = 'Error:'+err;
         });
-    //});
-    //p.then(function() {return ret});
-    //return ret;
+    };
+
+    // remove any existing div#status
+    let status = document.getElementById('status');
+    if (status) status.remove();
+    // inject the status top div
+    let div = document.createElement('div');
+    div.id = 'status';
+    div.style = 'position: fixed; top: 0; left: 0; width: 100%; height: 50px; background-color: rgba(255, 255, 255, 0.5); color: black; z-index: 9999;';
+    // add paragraph
+    let text = document.createElement('p');
+    text.id = 'text';
+    text.innerText = '';
+    div.appendChild(text);
+    // add input#id_login
+    let id_login = document.createElement('input');
+    id_login.id = 'id_login';
+    //id_login.type = 'text';
+    id_login.value = '';
+    div.appendChild(id_login);
+    // add input#id_page
+    let id_page = document.createElement('input');
+    id_page.id = 'id_page';
+    //id_page.type = 'text';
+    id_page.value = '';
+    div.appendChild(id_page);
+    // add input#url
+    let url = document.createElement('input');
+    url.id = 'url';
+    //url.type = 'text';
+    url.value = '';
+    div.appendChild(url);
+    // add status top div into the page
+    document.body.appendChild(div);
+    // show loading status
+    text.innerHTML = 'Loading...';
+    // define the api URL
+    let apiCall = csdomain+'/api1.0/isn/login.json?email='+email+'&password='+password;
+    fetch(apiCall).then(function(res) {
+        // wait for resonse
+        if (res.status !== 200) {
+            text.innerHTML = 'Protocol Error';
+        }
+        res.json().then(function(data) {
+            // show the response
+            if (data.status === 'success') {
+                text.innerHTML = 'Res:'+data.status;
+                id_login.value = data.id_login;
+                get_page(csdomain);
+            } else {
+                text.innerHTML = 'Res:'+data.status;
+            }
+        });
+    }).catch(function(err) {
+        // error
+        //
+        // If you are working on DEV, probably you are getting this error because your SSL cerificate is not tusted.
+        // In this case, go to https://127.0.0.1/ on other tab of the same browser, and choose to trust the certificate.
+        // After that, you can go back to this tab and try again.
+        // 
+        text.innerHTML = 'Error:'+err;
+    });
 }
 
 login.onclick = function() {
     // go to the page
-    chrome.tabs.update({url: 'https://www.amazon.com'});
+    chrome.tabs.update({url: 'https://www.linkedin.com'});
     // fired when tab is updated
     chrome.tabs.onUpdated.addListener(function openPage(tabID, changeInfo) {
         // tab has finished loading
@@ -61,7 +118,7 @@ login.onclick = function() {
                 {
                     target: {tabId: tabID, allFrames: true},
                     //code: 'e="123";',
-                    args: [ url, email.value, password.value ],
+                    args: [ csdomain, email.value, password.value ],
                     func: do_login
                 }, 
                 // Get the value returned by do_login.

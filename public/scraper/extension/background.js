b077ba974ec89c90c7eabfda71a61e01287fa087
leandrosardi <leandro.sardi@expandedventure.com>
2022-12-13 20:02:31 -0300

    #18

--- public/scraper/extension/background.js
+++ public/scraper/extension/background.js
@@ -1,30 +1,17 @@
-/*
-References about Chrome Extensions:
-- https://usefulangle.com/post/339/chrome-extension-create-page-scraper
-- https://stackoverflow.com/questions/19758028/chrome-extension-get-dom-content
-- https://developer.chrome.com/docs/extensions/reference/index.html
-Refernces about Upload files with AJAX:
-- https://pqina.nl/blog/set-value-to-file-input/
-*/
+// Reference: 
+// - https://levelup.gitconnected.com/how-to-use-background-script-to-fetch-data-in-chrome-extension-ef9d7f69625d
+// - https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers
 
 'use strict';
-
+/*
 //const csdomain = 'http://connectionsphere.com:3000';
 const csdomain = 'http://127.0.0.1:3000';
-
-let email = document.getElementById('email');
-let password = document.getElementById('password');
-let id_login = document.getElementById('id_login');
-let id_page = document.getElementById('id_page');
-let page_url = document.getElementById('page_url');
-let text = document.getElementById('text');
-let tab_id_before_popup = null; // current tab that I have to manipulate
+let id_login = null;
 
 // steps: how many times to scroll down
 // steps_length: how many pixels each step
 // delay_between_steps: how many milliseconds to wait between each step
 async function upload_page(steps=10, step_length=1000, delay_between_steps=1000) {
-alert('1');
     // scroll down to load AJAX content
     var i = 0;
     while (i < steps) {
@@ -70,14 +57,6 @@ page_url.value = 'https://github.com';
                             //if (page_content.length > maxsize) {
                             //    page_content = page_content.substring(0, maxsize);
                             //}
-
-                            /*
-                            text.innerHTML = "Saving page...";
-                            // save the HTML page in the download folder
-                            let blob = new Blob([page_content], {type: "application/html;charset=utf-8"});
-                            let objectURL = URL.createObjectURL(blob);
-                            chrome.downloads.download({ url: objectURL, filename: ('linkedin-scraper/data.html'), conflictAction: 'overwrite' });
-                            */
                            
                             text.innerHTML = "Uploading page...";
                             let fileInput = document.getElementById('file');
@@ -161,88 +140,26 @@ function get_page() {
         text.innerHTML = 'Error:'+err;
     });
 };
+*/
 
-// Login to CS
-function do_login() { 
-    // show logging in caption
-    text.innerHTML = 'Logging in.....';
-    // define the api URL
-    let apiCall = csdomain+'/api1.0/scraper/login.json?email='+email.value+'&password='+password.value;
-    fetch(apiCall).then(function(res) {
-        // wait for resonse
-        if (res.status !== 200) {
-            text.innerHTML = 'Protocol Error';
-        }
-        res.json().then(function(data) {
-            // show the response
-            if (data.status === 'success') {
-                text.innerHTML = data.status;
-                id_login.value = data.id_login;
-                get_page();
-            } else {
-                text.innerHTML = data.status;
-            }
-        });
-    }).catch(function(err) {
-        // error
-        //
-        // If you are working on DEV, probably you are getting this error because your SSL cerificate is not tusted.
-        // In this case, go to https://127.0.0.1/ on other tab of the same browser, and choose to trust the certificate.
-        // After that, you can go back to this tab and try again.
-        // 
-        text.innerHTML = 'Error:'+err;
-    });
-}
+console.log('background.js loaded');
 
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    //let tab = tabs[0];
+    console.log(tabs.length)
+});
 
-login.onclick = function() {
-    //do_login();
-    
-    function a() {
-        alert('a');
-    }
+//setTimeout(function() {
+//}, 1000);
 
-    let tid = parseInt(document.getElementById('tab_id').value);
-
-//    chrome.tabs.update({url: 'https://connectionsphere.com'});
-
-    // find a chrome tab by its id
-    chrome.tabs.query({}, function(tabs) {
-        for (let i=0; i<tabs.length; ++i) {
-            if (tabs[i].id == tid) {
-                chrome.tabs.update(tabs[i].id, {url: 'https://connectionsphere.com'});
-            }
-        }
-    });
 
 /*
-    chrome.tabs.onUpdated.addListener(function openPage(tid, changeInfo) {
-        chrome.scripting.executeScript(
-            {
-                target: {tabId: parseInt(tid), allFrames: true},
-                //args: [ csdomain, id_page.value ],
-                func: a
-            },
-            // Get the value returned by do_login.
-            // Reference: https://developer.chrome.com/docs/extensions/reference/scripting/#handling-results
-            (injectionResults) => {
-                // ...
-            }
-        );
-    });
-*/
-}
-
-
-popup.onclick = function() {
-    // go to the page
+//setTimeout(function() {
     chrome.tabs.query({active: true, currentWindow: true}, async function(tabs) {
         // get the tab id
-        let tab_id = tabs[0].id;
-        // open a new popup
-        let win1 = window.open('popup.html', 'win1', 'width=400,height=400');
-        win1.onload = function() {
-            win1.document.getElementById('tab_id').value = tab_id;
-        }    
+        var tab_id = tabs[0].id;
+        // go to the page
+        chrome.tabs.update({url: 'https://github.com'});
     });
-}
+//}, 1000);
+*/
